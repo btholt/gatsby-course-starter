3ed9113988c8c6e6b8405b3719a1d3b8c2fb3155
Brian Holt <btholt@gmail.com>
2021-04-27 20:40:49 -0700

    fix sharp config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,18 +8,19 @@ module.exports = {
       "this is",
       "a list of keywords and phrase",
       "that search engines",
-      "will index your page for"
-    ]
+      "will index your page for",
+    ],
   },
   //pathPrefix: "/my-repo-name", // if you're using GitHub Pages, put the name of the repo here with a leading slash
   plugins: [
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-layout`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/lessons`,
-        name: "markdown-pages"
-      }
+        name: "markdown-pages",
+      },
     },
     `gatsby-plugin-react-helmet`,
     {
@@ -34,11 +35,11 @@ module.exports = {
             options: {
               maxWidth: 800,
               linkImagesToOriginal: true,
-              sizeByPixelDensity: false
-            }
-          }
-        ]
-      }
-    }
-  ]
+              sizeByPixelDensity: false,
+            },
+          },
+        ],
+      },
+    },
+  ],
 };
